e8450ae9a1a6b91b90fefdac12cbc18c3df35f1b
plg-irk <nk-irk@mail.ru>
2021-07-16 00:03:22 +0800

    Реализация задания для кота

--- src/main/java/lesson1/Cat.java
+++ src/main/java/lesson1/Cat.java
@@ -1,27 +1,55 @@
 package lesson1;
 
-public class Cat {
-    public String name;
-    public String color;
-    public int age;
+public class Cat implements CanRunning {
+    final String name;
+    private int speedHeight;
+    private int speedLength;
+    private int maxHeight;
+    private int maxLength;
+    private boolean statusCompetitor;
 
     @Override
-    // Переопределили метод для корректного отображения информации по классу
     public String toString() {
         return "Cat{" +
                 "name='" + name + '\'' +
-                ", color='" + color + '\'' +
-                ", age=" + age +
+                ", speedHeight=" + speedHeight +
+                ", speedLength=" + speedLength +
+                ", maxHeight=" + maxHeight +
+                ", maxLength=" + maxLength +
+                ", status=" + statusCompetitor +
                 '}';
     }
 
-    public Cat(String name, String color, int age) {
+    public Cat(String name,
+               int speedHeight, int speedLength,
+               int maxHeight, int maxLength,
+               boolean status) {
+
         this.name = name;
-        this.color = color;
-        this.age = age;
+        this.statusCompetitor = status;
 
+        if (speedHeight < 0 || speedLength < 0 || maxHeight < 0 || maxLength < 0) {
+            System.out.println("Вы задали некорректные параметры");
+        } else {
+            this.speedHeight = speedHeight;
+            this.speedLength = speedLength;
+            this.maxHeight = maxHeight;
+            this.maxLength = maxLength;
+        }
+    }
 
-
-
+    public double running(RoadRun roadRun) {
+        double timeToOvercome = 0;
+        if (statusCompetitor) {
+            if (roadRun.lengthRoadRun < maxLength) {
+                timeToOvercome = roadRun.lengthRoadRun / speedLength;
+                System.out.println("Я кот, пробежал " + roadRun.lengthRoadRun + " за "
+                        + timeToOvercome);
+            } else {
+                statusCompetitor = false;
+                System.out.println("Я кот, я не могу пробежать " + roadRun.lengthRoadRun);
+            }
+        } else System.out.println("Кот не учавствует в преодалении предпятствий.");
+        return timeToOvercome;
     }
 }
--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -2,7 +2,24 @@ package lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Barsic", "white", 1);
+
+        RoadRun[] roadRuns = new RoadRun[3];
+
+        Cat cat = new Cat("Barsic",
+                10, 100,
+                5, 500, true);
+
+        int dict1 = ((int) (Math.random() * (10 - 1) + 1)) * 100;
+        roadRuns[0] = new RoadRun(dict1);
+        int dict2 = ((int) (Math.random() * (10 - 1) + 1)) * 100;
+        roadRuns[1] = new RoadRun(dict2);
+        int dict3 = ((int) (Math.random() * (10 - 1) + 1)) * 100;
+        roadRuns[2] = new RoadRun(dict3);
+
+
+        for (RoadRun roadRun: roadRuns) {
+            roadRun.getTimeToOvercomeRoadRun(cat);
+        }
 
 
     }
