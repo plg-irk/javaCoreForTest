19786bc71f4f25c6e423eed0232b65c234bcca71
plg-irk <nk-irk@mail.ru>
2021-08-02 12:35:09 +0800

    Домашнее задание №5 корр

--- src/main/java/lesson5/AppData.java
+++ src/main/java/lesson5/AppData.java
@@ -1,11 +1,12 @@
 package lesson5;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class AppData {
 
     private String[] header;
-    private int[][] data;
+    private Integer[][] data;
 
     public String[] getHeader() {
         return header;
@@ -15,32 +16,29 @@ public class AppData {
         this.header = header;
     }
 
-    public int[][] getData() {
+    public Integer[][] getData() {
         return data;
     }
 
-    public void setData(int[][] data) {
+    public void setData(Integer[][] data) {
         this.data = data;
     }
 
     public void read(File fileIn) {
-
         try (BufferedReader file = new BufferedReader(new
                 FileReader(fileIn))) {
             String tempString;
             int num = 0;
-            int[][] arrayInt = new int[2][3];
+            ArrayList<Integer[]> arrayList = new ArrayList<>();
             while ((tempString = file.readLine()) != null) {
                 String[] dataParamLine = tempString.split(";");
                 if (num == 0) {
                     this.setHeader(dataParamLine);
-                } else {
-                    for (int i = 0; i < dataParamLine.length; i++) {
-                        arrayInt[num - 1][i] = Integer.parseInt(dataParamLine[i]);
-                    }
-                }
+                } else
+                    arrayList.add(stringToIntegerArray(dataParamLine));
                 num += 1;
             }
+            Integer[][] arrayInt = arrayList.toArray(new Integer[][]{{}});
             this.setData(arrayInt);
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,17 +46,30 @@ public class AppData {
     }
 
     public void save(File fileOut) {
-
         try (PrintWriter file = new PrintWriter(new FileWriter(fileOut))) {
-            file.println(header[0] + ";" +
-                    header[1] + ";" +
-                    header[2]);
-            for (int[] d : data) {
-                file.println(d[0] + ";" + d[1] + ";" + d[2]);
+            file.println(saveStringToFile(header));
+            for (Integer[] d : data) {
+                file.println(saveStringToFile(d));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private Integer[] stringToIntegerArray(String[] array) {
+        ArrayList<Integer> arrayTemp = new ArrayList<>();
+        for (String arr : array)
+            arrayTemp.add(Integer.parseInt(arr));
+        return arrayTemp.toArray(new Integer[]{});
+    }
+
+    private <T> String saveStringToFile(T[] arrayIn) {
+        String result = "";
+        for (int i = 0; i < arrayIn.length; i++) {
+            result += arrayIn[i];
+            if (i != arrayIn.length - 1)
+                result += ";";
+        }
+        return result;
     }
 }
